0e1115c0f22cc6aebdd6961baabf03dd203f2680
jbish94 <jordanbishop97@gmail.com>
2025-11-12 10:58:28 -0700

    Update location_service_stub.dart

--- lib/services/location_service_stub.dart
+++ lib/services/location_service_stub.dart
@@ -1,4 +1,10 @@
 class LocationService {
-  Future<void> init() async {}
-  Future<String> getLocation() async => 'Unavailable';
+  static final instance = LocationService._internal();
+  LocationService._internal();
+
+  Future<dynamic> getCurrentLocation() async =>
+      {'latitude': 0.0, 'longitude': 0.0};
+
+  Future<String> getAddressFromCoordinates(double lat, double lon) async =>
+      'Unavailable';
 }
