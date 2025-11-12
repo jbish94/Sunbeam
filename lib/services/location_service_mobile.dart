6034b5d31fd4d6c51437b14bb3d8ff3e1caed110
jbish94 <jordanbishop97@gmail.com>
2025-11-12 12:13:00 -0700

    Update location_service_mobile.dart

--- lib/services/location_service_mobile.dart
+++ lib/services/location_service_mobile.dart
@@ -1,12 +1,5 @@
 import 'package:geolocator/geolocator.dart';
 
-class PositionData {
-  final double latitude;
-  final double longitude;
-  final String address;
-  PositionData(this.latitude, this.longitude, this.address);
-}
-
 class LocationService {
   static final instance = LocationService._internal();
   LocationService._internal();
@@ -23,13 +16,13 @@ class LocationService {
   Future<void> openLocationSettings() async => Geolocator.openLocationSettings();
   Future<void> openAppSettings() async => Geolocator.openAppSettings();
 
-  Future<PositionData> getCurrentLocation() async {
+  Future<Map<String, dynamic>> getCurrentLocation() async {
     await Geolocator.requestPermission();
     final pos = await Geolocator.getCurrentPosition();
     final lat = pos.latitude;
     final lon = pos.longitude;
     final address = '$lat, $lon';
-    return PositionData(lat, lon, address);
+    return {'latitude': lat, 'longitude': lon, 'address': address};
   }
 
   Future<String> getAddressFromCoordinates(double lat, double lon) async {
@@ -39,11 +32,14 @@ class LocationService {
   Future<String> getTimezone([double? lat, double? lon]) async =>
       DateTime.now().timeZoneName;
 
-  Future<void> saveLocationToSupabase(PositionData position) async {
-    print('Saving location to Supabase: ${position.latitude}, ${position.longitude}');
+  Future<bool> saveLocationToSupabase(dynamic position) async {
+    final lat = (position['latitude'] ?? 0).toDouble();
+    final lon = (position['longitude'] ?? 0).toDouble();
+    print('Saving location to Supabase: $lat, $lon');
+    return true;
   }
 
-  Future<PositionData> getCurrentLocationFromSupabase() async {
-    return PositionData(0.0, 0.0, 'Unknown');
+  Future<Map<String, dynamic>> getCurrentLocationFromSupabase() async {
+    return {'latitude': 0.0, 'longitude': 0.0, 'address': 'Unknown'};
   }
 }
